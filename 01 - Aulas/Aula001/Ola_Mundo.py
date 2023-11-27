e1b71a90ddf88363ee53f72f7e065b963d8f1ff1
macielthiago <maciel.rosari54@gmail.com>
2023-11-27 09:14:50 -0400

    Aula 1 + desafios

--- 01 - Aulas/Aula001/Ola_Mundo.py
+++ 01 - Aulas/Aula001/Ola_Mundo.py
@@ -1,4 +1,4 @@
-print("Olá, Mundo!")
-
-msg = 'Olá, Mundo!'
-print(msg)
+print('Olá, Mundo')
+print(7 + 4)
+print('7'+'4')  # o '+' serve para concatenar
+print('Olá', 5)  # aqui o sinal de + não serve, então usa-se a vírgula no lugar
